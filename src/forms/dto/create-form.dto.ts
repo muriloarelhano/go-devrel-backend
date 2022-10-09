44145aacc63ac18b81337d042eb579cff7c21e14
Murilo Esteca Arelhano <51941771+muriloarelhano@users.noreply.github.com>
2022-10-09 15:46:10 -0400

    chore: update forms schema

--- src/forms/dto/create-form.dto.ts
+++ src/forms/dto/create-form.dto.ts
@@ -1,24 +1,19 @@
-import { ApiProperty } from '@nestjs/swagger';
-import {
-    IsString,
-    IsNotEmpty,
-    IsArray,
-} from 'class-validator'
-import { QuestionResponse } from '../entities/question-response.entity';
+import { ApiProperty } from "@nestjs/swagger";
+import { IsString, IsNotEmpty } from "class-validator";
+import { QuestionResponse } from "../entities/question-response.entity";
 
 export class CreateFormDto {
-    @IsString()
-    @IsNotEmpty()
-    @ApiProperty()
-    stage: string;
+  @IsString()
+  @IsNotEmpty()
+  @ApiProperty()
+  stage: string;
 
-    @IsString()
-    @IsNotEmpty()
-    @ApiProperty()
-    formIdentifier: string;
+  @IsString()
+  @IsNotEmpty()
+  @ApiProperty()
+  formIdentifier: string;
 
-    @IsArray()
-    @IsNotEmpty()
-    @ApiProperty()
-    responses: QuestionResponse[];
+  @IsNotEmpty()
+  @ApiProperty()
+  responses: QuestionResponse;
 }
